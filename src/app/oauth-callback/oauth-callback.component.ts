f073a7b81ee784020754143b1f55ae88eced0235
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-04-14 17:10:01 +0530

    Handled error on digilocker callback

--- src/app/oauth-callback/oauth-callback.component.ts
+++ src/app/oauth-callback/oauth-callback.component.ts
@@ -15,6 +15,8 @@ import { IImpressionEventInput, IInteractEventInput } from '../services/telemetr
 })
 export class OauthCallbackComponent implements OnInit {
   baseUrl: string;
+  isError = false;
+  errorCode: string;
   constructor(
     private activatedRoute: ActivatedRoute,
     private generalService: GeneralService,
@@ -30,6 +32,10 @@ export class OauthCallbackComponent implements OnInit {
       console.log("params-------->", params);
       if (params.code) {
         this.getUserData(params.code);
+      }
+      if (params.error) {
+        this.isError = true;
+        this.errorCode = params.error;
       }
     });
   }
