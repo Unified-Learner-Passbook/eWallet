e55d83b5096c8c4cbd506ddf02760d3067973826
apurvaubade <apurva.bade@tekditechnologies.com>
2023-04-17 17:13:49 +0530

    changes for default & dark theme

--- src/app/theme.config.ts
+++ src/app/theme.config.ts
@@ -9,8 +9,20 @@ export const THEMES = {
     navLabelColor: "#B275FF",
     headerColor: "#4A227C",
     headerLinkColor: "#F4ECFF",
-    primaryTextColor: "#000000",
+    primaryTextColor: "#494949",
     secondaryTextColor: "#8D9091",
+    
+
+
+    titleColor:"#5526f5",
+    logoPrimaryTextColor:"#1b1818",
+    logoHeadlineColor:"#79787c",
+    logoSecondaryTextColor:"#ee8908",
+    toggleColor: "#5526f5",
+    btnPrimaryColor:"#060461"
+
+
+
   },
   dark: {
     primaryColor: "#060461",
@@ -22,6 +34,17 @@ export const THEMES = {
     headerColor: "#313131",
     primaryTextColor: "#FFFFFF",
     secondaryTextColor: "#c3c3c3",
+    
+    titleColor:"#fbfbfb",
+
+    logoPrimaryTextColor:"#ffffff",
+    logoHeadlineColor:"#ffffff",
+    logoSecondaryTextColor:"#c3c3c3",
+    toggleColor: "#ffffff",
+    btnPrimaryColor:"#f07d00"
+
+
+
   }
 };
 
