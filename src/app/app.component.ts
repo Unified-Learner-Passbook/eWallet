b7b3b3b2ae76463a42a8f90f779f511ab4004e57
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-05-10 11:52:25 +0530

    Issue #ULP-000 fix: Enabled Telemetry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class AppComponent implements OnInit {
       EkTelemetry.getFingerPrint((deviceId, components, version) => {
         console.log('deviceId', deviceId);
         this.telemetryService.did = deviceId;
-        // this.telemetryService.initializeTelemetry();
+        this.telemetryService.initializeTelemetry();
       });
     }
   }
